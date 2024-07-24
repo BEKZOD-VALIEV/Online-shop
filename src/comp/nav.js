29e81d651ce5793fc41f7c719bc444429828f6a1
Bekzod Valiev <108138720+BEKZOD-VALIEV@users.noreply.github.com>
2024-07-24 16:22:12 +0500

    added images and new search bar

--- src/comp/nav.js
+++ src/comp/nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { MdLocalShipping } from "react-icons/md";
 import './nav.css'
+import { CiSearch } from "react-icons/ci";
 
 const Nav = () => {
   return (
@@ -14,6 +15,15 @@ const Nav = () => {
             <p>Free Shipping When Shopping upto $1000</p>
           </div>
         </div>
+        <div className="mid_header">
+            <div className="logo">
+              <img src="image/logo.webp" alt="" />
+            </div>
+            <div className="search_box">
+              <input type="text" value="" placeholder="search" />
+              <button><CiSearch /></button>
+            </div>
+          </div>
       </div>
     </>
   );
