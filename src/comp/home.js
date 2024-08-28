f89544fb87c942bad79aceef14055cffa089cad5
Bekzod Valiev <108138720+BEKZOD-VALIEV@users.noreply.github.com>
2024-08-28 21:27:10 +0500

    added news img

--- src/comp/home.js
+++ src/comp/home.js
@@ -139,6 +139,18 @@ const Home = () => {
               <div className="box">
                 <img src="image/Multi-Banner-1.avif" alt="" />
               </div>
+              <div className="box">
+                <img src="image/Multi-Banner-2.avif" alt="" />
+              </div>
+            </div>
+            <div className="right_box">
+              <div className="top">
+                <img src="image/Multi-banner-3.webp" alt="" />
+                <img src="image/Multi-banner-4.avif" alt="" />
+              </div>
+              <div className="bottom">
+                <img src="image/Multi-Banner-5.webp" alt="" />
+              </div>
             </div>
           </div>
         </div>
