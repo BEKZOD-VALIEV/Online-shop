d8fe0b7de0bdea7bab3345219c549fcae8ec3e72
Bekzod Valiev <108138720+BEKZOD-VALIEV@users.noreply.github.com>
2024-09-11 19:00:11 +0500

    added Shop links

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import React, { useState } from 'react'
-import Nav from './comp/nav'
-import {BrowserRouter} from 'react-router-dom'
-import Rout from './comp/rout'
-import Footer from './comp/footer'
-import Homeproduct from './comp/home_product'
+import React, { useState } from "react";
+import Nav from "./comp/nav";
+import { BrowserRouter } from "react-router-dom";
+import Rout from "./comp/rout";
+import Footer from "./comp/footer";
+import Homeproduct from "./comp/home_product";
 
 const App = () => {
   //Shop Page product
-  const [shop, setShop] = useState(Homeproduct)
+  const [shop, setShop] = useState(Homeproduct);
+  //Shop Search Filter
+  const [search, setSearch] = useState("");
+  //Shop category filter
+  const Filter = (x) => {
+    const catefilter = Homeproduct.filter((product) => {
+      return product.cat === x;
+    });
+    setShop(catefilter);
+  };
+  const allcatefilter = () => {
+    setShop(Homeproduct);
+  };
+  //Shop Search Filter
+  const searchlength = (search || []).length === 0;
+  const searchproduct = () =>
+  {
+  if (searchlength) {
+    alert("Please Search Something !");
+    setShop(Homeproduct)
+  }
+  else
+  {
+      const searchfilter = Homeproduct.filter((x) => {
+        return x.cat === search;
+      });
+      setShop(searchfilter);
+  }
+}
   return (
     <>
-    <BrowserRouter>
-    <Nav />
-    <Rout shop={shop} />
-    <Footer />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Nav
+          search={search}
+          setSearch={setSearch}
+          searchproduct={searchproduct}
+        />
+        <Rout shop={shop} Filter={Filter} allcatefilter={allcatefilter} />
+        <Footer />
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
